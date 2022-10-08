1d8df490dc1597e83b341e27753873714b95fe8c
Benjamin Q <benking3502@yahoo.com>
2022-10-08 18:14:15 -0400

    Add CSS styling and icons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './style.css';
 import { InformationItemManager } from './informationitemmanager.js'
 import GitHubLogo from './images/GitHub-Mark-32px.png';
+import CheckListLogo from './icons/clipboard-list.svg';
+import AddTodoIcon from './icons/plus.svg';
 
 console.log('this works!');
 console.log('epic');
 
+// Setup GitHub logo
 const gitHubLogoElem = document.querySelector('img.github-logo');
 gitHubLogoElem.setAttribute('src', GitHubLogo);
+
+// Setup icons
+const checkListLogoElem = document.querySelector('img.header-logo');
+checkListLogoElem.setAttribute('src', CheckListLogo);
+const addTodoElem = document.querySelector('.add-todo-button-icon');
+addTodoElem.setAttribute('src', AddTodoIcon);
